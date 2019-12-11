7dcd78f1d24f148f73e8a5d4740a72b57ec8c1c2
Bhavin <bhavin@code.com>
2019-12-11 12:08:46 +0530

    init code

---
+++ main.js
@@ -0,0 +1,29 @@
+const { Worker, isMainThread, workerData } = require('worker_threads');
+
+if (isMainThread) {
+    console.log("this is the main thread")
+    // console.log(__dirname);
+    let workerone = new Worker('./workerone.js', { workerData: 'original.jpg' });
+    let workertwo = new Worker('./workertwo.js', { workerData: __dirname + '/original.jpg' });
+
+    workertwo.on('message', (data) => {
+        console.log("message", data);
+    })
+
+    workertwo.on('error', (err) => {
+        console.error(err);
+    })
+
+    workerone.on('message', (data) => {
+        console.log("message", data)
+    })
+
+    workerone.on('error', (err) => {
+        console.log(err);
+    })
+
+    workerone.on('exit', (code) => {
+        if (code != 0)
+            console.error(`Worker stopped with exit code ${code}`)
+    })
+}
---
+++ workerone.js
@@ -0,0 +1,14 @@
+const imagemagick = require('imagemagick');
+const path = require("path");
+const { workerData, parentPort } = require('worker_threads');
+
+let destSource = path.join(__dirname, workerData);
+let destDest = path.join(__dirname, 'one.jpg');
+
+let optionObj = [destSource, '-resize', '100x100', destDest]
+console.log("workerData",optionObj);
+imagemagick.convert(optionObj, (err, stdout) => {
+    if (err) throw err;
+    parentPort.postMessage({ fileName: destDest, status: 'Done' })
+})
+
